b3084dc88463bd0b7c0a25696d1ef4ee6184ae81
Uche05 <uchekpadeuwa@gmail.com>
2025-04-06 18:06:28 +0100

    change hosts in settings.py

--- cwyproject/settings.py
+++ cwyproject/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-&bhvggipvo)hexz=!5s71gsv42t+_b41h90*@n+vj9++#^bnho
 # DEBUG = "PRODUCTION" not in os.environ
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'django-blog-uckj.herokuapp.com/',]
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com/',]
 
 
 # Application definition

42e2fbd24f5685c360f4dba21ac7b3b9cee381d3
Uche05 <uchekpadeuwa@gmail.com>
2025-08-06 14:18:33 +0100

    set new host

--- cwyproject/settings.py
+++ cwyproject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-&bhvggipvo)hexz=!5s71gsv42t+_b41h90*@n+vj9++#^bnho
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'cwydjango-224468287572.herokuapp.com', 'cleaningwithyink.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-blog-uckj-1f47f900818a.herokuapp.com/', 'cleaningwithyink.com']
 
 
 # Application definition
